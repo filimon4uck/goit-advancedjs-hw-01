98bef84e7063cf0c0edd412d2cf25ccf46e1c5b3
filimon4uck <134231431+filimon4uck@users.noreply.github.com>
2023-11-02 18:21:11 +0100

    add gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,25 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
 import { galleryItems } from './gallery-items';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
-console.log(galleryItems);
+function createMurkup(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image"src="${preview}"  alt="${description}" />
+   </a>
+</li>`
+    )
+    .join('');
+}
+const galleryEl = document.querySelector('.gallery');
+galleryEl.insertAdjacentHTML('afterbegin', createMurkup(galleryItems));
 
-console.log('hello ');
+new SimpleLightbox('.gallery .gallery__link', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
