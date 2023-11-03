7adb446adb9eec24b5496e48db58aec04e5a9275
filimon4uck <134231431+filimon4uck@users.noreply.github.com>
2023-11-03 20:53:28 +0100

    awdaw

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,21 +2,21 @@ import { throttle } from 'lodash';
 const formEl = document.querySelector('.feedback-form');
 const KEY_FORM_VALUES = 'feedback-form-state';
 
-let formDataFromLS = JSON.parse(localStorage.getItem(KEY_FORM_VALUES)) ?? {};
-formEl.email.value = formDataFromLS.email ?? '';
-formEl.message.value = formDataFromLS.message ?? '';
+let formData = JSON.parse(localStorage.getItem(KEY_FORM_VALUES)) ?? {};
+
+formEl.email.value = formData.email || '';
+formEl.message.value = formData.message || '';
 
 formEl.addEventListener('submit', evt => {
   evt.preventDefault();
   localStorage.clear();
-  console.log(formDataFromLS);
+  console.log(formData);
   evt.currentTarget.reset();
-  formDataFromLS = {};
+  formData = {};
 });
 
 formEl.addEventListener('input', throttle(inputHandler, 500));
 function inputHandler({ target } = evt) {
-  const formData = JSON.parse(localStorage.getItem(KEY_FORM_VALUES)) ?? {};
   formData[target.name] = target.value;
   localStorage.setItem(KEY_FORM_VALUES, JSON.stringify(formData));
 }
